4dbabe2b16303ebf19ea5bd0f6f2957951d103a7
duuya49 <eabdollah.ae@gmail.com>
2025-12-19 22:56:54 +0100

    Fix: Add pathPrefix and URL filters for GitHub Pages subdirectory

--- eleventy.config.js
+++ eleventy.config.js
@@ -13,6 +13,7 @@ module.exports = function (eleventyConfig) {
     });
 
     return {
+        pathPrefix: "/abdellah-portfolio/",
         dir: {
             input: "src",
             output: "_site",
